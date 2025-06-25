ba3c1a003081058025c90f2447690ada9a4c5777
Kunal pipaliya <Kunal.pipaliya92@gmail.com>
2025-06-25 12:44:57 +0530

    added high-low model number

--- test_flipkart.py
+++ test_flipkart.py
@@ -13,6 +13,7 @@ driver.implicitly_wait(10)
 
 print("mobile searched")
 
+# Price
 xpath_price_list = "//*[@class='Nx9bqj _4b5DiR']"
 price_element = driver.find_elements(By.XPATH, xpath_price_list)
 prices = []
@@ -30,4 +31,23 @@ if prices:
 else:
     print("No prices found.")
     
+# Model find name
 
+xpath_model_list = "//*[@class='KzDlHZ']"
+model_element = driver.find_elements(By.XPATH, xpath_model_list)
+
+model = []
+
+for mo in model_element:
+    textmo = mo.text.strip()
+    if textmo:
+        model.append(textmo)
+    
+if model:
+    high = max(model)
+    low = min(model)
+    print(high, "Highest model number")
+    print(low, "Lowest model number")
+
+else:
+    print("No model found.")
